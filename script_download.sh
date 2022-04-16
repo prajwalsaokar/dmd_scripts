e8071cfb5e1fb3fd34f1db5f44cd316b4f65f8bf
Prajwal Saokar <prajwalsaokar@gmail.com>
2022-04-16 01:51:32 -0400

    added script to download and extract the repo

---
+++ script_download.sh
@@ -0,0 +1,7 @@
+wget https://github.com/prajwalsaokar/dmd_scripts/archive/main/dmd_scripts.tar.gz 
+tar -xf dmd_scripts.tar.gz 
+rm dmd_scripts.tar.gz 
+mv dmd_scripts-main/* ./ 
+rm -Rf dmd_scripts-main
+rm README.md  
+rm  script_download.sh 
