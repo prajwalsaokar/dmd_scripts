67e28af201ae384091d571ce1f60b220e227514f
Prajwal Saokar <prajwalsaokar@gmail.com>
2022-04-19 19:59:48 -0400

    correct outfile with number of collsions instead of runs

--- dmd_run.sh
+++ dmd_run.sh
@@ -6,6 +6,6 @@ sed -i "s/temp_0195/temp_0$(echo $reduced_temperature_value*1000 / 1 | bc)/" qfi
 sed -i "s/out_0195/out_0$(echo reduced_temperature_value*1000 | bc)/" qfile/subscript.sh
 sed -i "s/600/$number_of_runs/" qfile/subscript.sh
 
-printf "%s\n" ${reduced_temperature_value}D0 $number_of_runs > temp_0$(echo $reduced_temperature_value*1000 / 1 | bc)
+printf "%s\n" ${reduced_temperature_value}D0 $number_collisions > temp_0$(echo $reduced_temperature_value*1000 / 1 | bc)
 cd qfile
 bash subscript.sh
